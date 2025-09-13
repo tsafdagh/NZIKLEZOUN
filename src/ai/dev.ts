cc1b89deb003303da10b073742afae7647a9a2c4
Cedrick Tsafack Dagha <tsafackdcedrick@gmail.com>
2025-09-13 11:52:48 +0000

    ca ne fonctionne pas: Error: An unexpected response was received from th

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,5 +1,5 @@
 import { config } from 'dotenv';
 config();
 
-import '@/ai/flows/generate-educational-video-script.ts';
-import '@/ai/flows/generate-video-from-script.ts';
+import '@/ai/flows/generate-educational-video-script';
+import '@/ai/flows/generate-video-from-script';
